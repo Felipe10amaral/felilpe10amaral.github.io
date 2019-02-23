31b2a3fe18fea6548696b3221959acfeaf484df6
Felipe Amaral <38993434+Felipe10amaral@users.noreply.github.com>
2019-02-22 22:02:34 -0300

    fatorial

---
+++ ParImpar.js
@@ -0,0 +1,55 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+  
+  fatorial, par , email e valido ou não */
+
+
+
+
+
+valor = prompt("entre com numero")
+
+  var resultado = par()
+
+ function par(){
+  
+  var teste = valor % 2;
+
+  
+  if( teste  == 0)
+    
+      alert('par')
+    
+  
+  else
+    
+      alert('impar')
+    
+}
+
+
+
+
+
+
+/*
+Exception: SyntaxError: redeclaration of const numero
+@Scratchpad/1:1:1
+*/
+/*
+Exception: SyntaxError: redeclaration of const numero
+@Scratchpad/1:1:1
+*/
+/*
+Exception: SyntaxError: redeclaration of const numero
+@Scratchpad/1:1:1
+*/
+/*
+Exception: SyntaxError: redeclaration of const numero
+@Scratchpad/1:1:1
+*/
